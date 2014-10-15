4d2f7e69c064f2669453aecb8e595276a5b89205
Jennings Anderson <jennings.anderson@gmail.com>
2014-10-15 16:29:21 -0600

    Added a new function called "say bye"

--- sample_code.rb
+++ sample_code.rb
@@ -13,6 +13,10 @@ class MyClass
     print "Hello, my name is: #{@name}"
   end
   
+  def say_bye
+    print "Goodbye!"
+  end
+  
 end
 
 if __FILE__ == $0
@@ -20,5 +24,7 @@ if __FILE__ == $0
   myclass = MyClass.new( name: "Jennings" )
   
   myclass.say_hi
+  
+  myclass.say_bye
   
 end
