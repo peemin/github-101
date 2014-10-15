029ee001f25f0c72fea106b8e9a4275b5354c509
Jennings Anderson <jennings.anderson@gmail.com>
2014-10-15 16:28:37 -0600

    Create sample_code.rb

---
+++ sample_code.rb
@@ -0,0 +1,24 @@
+#
+# A sample Ruby File to show why we use GitHub to view code
+#
+#
+
+class MyClass
+
+  def initialize(args)
+    @name = args[:name]
+  end
+  
+  def say_hi
+    print "Hello, my name is: #{@name}"
+  end
+  
+end
+
+if __FILE__ == $0
+
+  myclass = MyClass.new( name: "Jennings" )
+  
+  myclass.say_hi
+  
+end
